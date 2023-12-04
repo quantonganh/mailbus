b8d19f78cdf1349db2f6f48a8049d91d3f2d28ff
Quan Tong <quantonganh@gmail.com>
2023-12-04 15:10:27 +0700

    define Config

---
+++ config.go
@@ -0,0 +1,37 @@
+package mailbus
+
+// Config represents the main config
+type Config struct {
+	DB struct {
+		Path string
+	}
+
+	HTTP struct {
+		Addr string
+	}
+
+	SMTP struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+	}
+
+	Newsletter struct {
+		From      string
+		Frequency int
+		Cron      struct {
+			Spec string
+		}
+		Product struct {
+			Name string
+		}
+		HMAC struct {
+			Secret string
+		}
+	}
+
+	Sentry struct {
+		DSN string
+	}
+}
